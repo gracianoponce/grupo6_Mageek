851a3869da5299d5c1983cd165c391e07591ba6d
nagykorte <nicolasmatray@hotmail.com>
2020-07-09 18:08:33 -0300

    tying backend

--- controllers/product.js
+++ controllers/product.js
@@ -1,56 +1,53 @@
 const fs = require("fs");
 const path = require("path");
-const pathDB = path.join(
-   __dirname,
-   '..',
-   'data',
-   'products.json'
-);
-
+const pathDB = path.join(__dirname, "..", "data", "products.json");
+const { Categorias, Productos } = require("../database/models");
 
 const controller = {
-   none: (req, res, next) => {
-      res.redirect("/");
-   },
-   product: (req, res) => {
-      const products = JSON.parse(fs.readFileSync(pathDB)); // load database
-      const product = products[req.params.id];
-      console.log(product);
+    none: async function (req, res, next) {
+        res.redirect("/");
+    },
+    product: async function (req, res) {
+       // load database
+       // load product
+       const product = await Productos.findByPk(req.params.id, {
+          include: [{
+             model: Categorias,
+             as: "categorias"
+       }]});
 
-      res.render("detalle-producto", {
-         product: product,
-      });
-   },
-   create: (req, res, next) => {
-      //goes to add page, GET
-      // middleware should check admin
-      res.render("addProduct");
-   },
-   save: function (req, res, next) {
-      // saves the product, POST
-      //let products = JSON.parse(fs.readFileSync(pathDB)); // load database
-      let products = JSON.parse(
-         fs.readFileSync(
-            path.join(__dirname, "..", "data", "products.json")
-         )
-      );
-      //handle the req
-      let product = {
-         id: 102,
-         name: req.body.name,
-         price: req.body.price,
-         description: req.body.description,
-         usage: req.body.usage,
-         flavortext: req.body.flavortext,
-      };
-      console.log(req.body);
+        res.render("detalle-producto", {
+            product: product,
+        });
+    },
+    create: (req, res, next) => {
+        //goes to add page, GET
+        // middleware should check admin
+        res.render("addProduct");
+    },
+    save: function (req, res, next) {
+        // saves the product, POST
+        //let products = JSON.parse(fs.readFileSync(pathDB)); // load database
+        let products = JSON.parse(
+            fs.readFileSync(path.join(__dirname, "..", "data", "products.json"))
+        );
+        //handle the req
+        let product = {
+            id: 102,
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+            usage: req.body.usage,
+            flavortext: req.body.flavortext,
+        };
+        console.log(req.body);
 
-      console.log(product);
+        console.log(product);
 
-      // maybe check something here?
-      products.push(product);
-      fs.writeFileSync(pathDB,JSON.stringify(products, null, 4)); //save, confirm, redirect with 3 seconds.
-      res.send(req.body);
-   },
+        // maybe check something here?
+        products.push(product);
+        fs.writeFileSync(pathDB, JSON.stringify(products, null, 4)); //save, confirm, redirect with 3 seconds.
+        res.send(req.body);
+    },
 };
 module.exports = controller;
---
+++ wireframes/datos_de_migracion.md
@@ -0,0 +1,5 @@
+1. Blisters
+2. Cartas
+3. Dados
+4. Folios
+5. Packs
